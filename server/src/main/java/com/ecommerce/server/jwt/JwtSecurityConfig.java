d416c383bdc57447a9040d76b20753a50c16403e
leesh235 <leeshgh235@gmail.com>
2021-12-03 22:36:19 +0900

    setting jwt

---
+++ server/src/main/java/com/ecommerce/server/jwt/JwtSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ecommerce.server.jwt;
+
+import com.ecommerce.server.jwt.JwtFilter;
+import com.ecommerce.server.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+//커스텀 JwtFilter를 security 로직에 적용
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity http){
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
---
+++ server/src/main/java/com/ecommerce/server/security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.ecommerce.server.config;
+
+import com.ecommerce.server.jwt.JwtAccessDeniedHandler;
+import com.ecommerce.server.jwt.JwtAuthenticationEntryPoint;
+import com.ecommerce.server.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+
+                //excoption handler에 커스텀 클래스 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                //세션을 사용하지 않기 때문에 세션설정을 stateless로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                //auth 관련 api는 토큰없이 접근가능, 나머지는 접근불가
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+
+                //커스텀 JwtFilter 적용
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+    }
+}
