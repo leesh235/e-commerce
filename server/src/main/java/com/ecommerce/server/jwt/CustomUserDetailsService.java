1dd5b020b5d55641f82bd8ff807ebf2f2adb3ecd
leesh235 <leeshgh235@gmail.com>
2021-12-04 19:09:20 +0900

    add UserDetail object(security)

---
+++ server/src/main/java/com/ecommerce/server/jwt/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.ecommerce.server.jwt;
+
+import com.ecommerce.server.domain.AuthDomain;
+import com.ecommerce.server.repository.mapper.MybatisAuthRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MybatisAuthRepository mybatisAuthRepository;
+
+    //db에서 존재여부를 확인 후 userdetails 객체로 반환
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        return mybatisAuthRepository.Select(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(email + "-> 데이터베이스에서 찾을 수 없습니다."));
+    }
+
+    //userdetails 객체 생성
+    private UserDetails createUserDetails(AuthDomain authDomain){
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(authDomain.getAuthority().toString());
+
+        User user = new User(
+                String.valueOf(authDomain.getUserId()),
+                //security version up에 따라 패스워드 앞에 "{noop}"을 붙여줘야한다
+                "{noop}" + authDomain.getPassword(),
+                Collections.singleton(grantedAuthority));
+
+        return user;
+    }
+
+}
