6689baf2d299aeeb4be50b9a7d84cc8a09d474b9
leesh235 <leeshgh235@gmail.com>
2021-12-04 19:14:02 +0900

    add jwt logic

--- server/src/main/java/com/ecommerce/server/service/AuthService.java
+++ server/src/main/java/com/ecommerce/server/service/AuthService.java
@@ -2,34 +2,40 @@ package com.ecommerce.server.service;
 
 import com.ecommerce.server.domain.AuthDomain;
 import com.ecommerce.server.dto.AuthDto;
+import com.ecommerce.server.jwt.TokenProvider;
 import com.ecommerce.server.repository.mapper.MybatisAuthRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
-    @Autowired
-    MybatisAuthRepository mybatisAuthRepository;
 
-    public List<AuthDomain> findAll(){
-        final List<AuthDomain> authList = mybatisAuthRepository.SelectAll();
-
-        return authList;
-    }
+    private final MybatisAuthRepository mybatisAuthRepository;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final TokenProvider tokenProvider;
 
     public void signUp(AuthDto.signUpDto dto){
-        AuthDomain authDomain = new AuthDomain();
-        authDomain.patchSignUpData(dto);
+        AuthDomain authDomain = new AuthDomain(dto.getEmail(), dto.getName(), dto.getPassword(), "USER");
         mybatisAuthRepository.Save(authDomain);
     }
 
-    public AuthDto.userLogInDto logIn(AuthDto.logInDto dto){
-        AuthDomain authDomain = new AuthDomain();
-        authDomain.patchLogInData(dto);
-        mybatisAuthRepository.Select(authDomain);
-        AuthDto.userLogInDto user = new AuthDto.userLogInDto(mybatisAuthRepository.Select(authDomain));
+    public AuthDto.tokenDto logIn(AuthDto.logInDto dto){
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword());
+
+        Authentication authentication =
+                authenticationManagerBuilder
+                        .getObject()
+                        .authenticate(authenticationToken);
+
+        AuthDto.tokenDto user = tokenProvider.generateTokenDto(authentication);
+
         return user;
     }
 
