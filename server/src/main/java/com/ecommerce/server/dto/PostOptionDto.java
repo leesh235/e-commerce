02309ce4b62a953d15bbe94141c7aac71201ed10
leesh235 <leeshgh235@gmail.com>
2022-01-05 23:46:24 +0900

    add postOptionDto

---
+++ server/src/main/java/com/ecommerce/server/dto/PostOptionDto.java
@@ -0,0 +1,31 @@
+package com.ecommerce.server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class PostOptionDto {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class createPostOptionDto{
+        private String optionName;
+        private long optionPrice;
+        private long optionAmount;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class getPostOptionDto{
+        private long postOptionId;
+        private String optionName;
+        private long optionPrice;
+        private long optionAmount;
+    }
+
+}
